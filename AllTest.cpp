127595e1891d5ff131efbb3d63d669f362fc0030
whisky-12 <1756522300@qq.com>
2022-11-03 10:13:39 +0800

    首次测试文件提交

---
+++ AllTest.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namesapce std;
+int main(){
+    // 测试部分 （测试者可一次添加输出与注释）
+    cout<<"Hello World"<<endl; // example - 2022/11/03
+
+    cout<<"xxxxx"<<endl;// test_name - xx/xx/xx
+    return 0;
+}
