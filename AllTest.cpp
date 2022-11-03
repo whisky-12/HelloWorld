e72d1aa71a3bc570939d1c371abad6a5a7263b52
whisky-12 <79358023+whisky-12@users.noreply.github.com>
2022-11-03 11:22:24 +0800

    Update AllTest.cpp
    修改注释

--- AllTest.cpp
+++ AllTest.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namesapce std;
 int main(){
-    // 测试部分 （测试者可添加输出与注释）
+    // 测试部分 （测试者可依次添加输出与注释）
     cout<<"Hello World"<<endl; // example - 2022/11/03
 
     cout<<"xxxxx"<<endl;// test_name - xx/xx/xx
