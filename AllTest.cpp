faec4a95a0bdb9a4d20a1e5153bdb0b2e41f56d6
whisky-12 <1756522300@qq.com>
2022-11-06 15:11:35 +0800

    修改测试

--- AllTest.cpp
+++ AllTest.cpp
@@ -2,7 +2,7 @@
 using namesapce std;
 int main(){
     // 测试部分 （测试者可依次添加输出与注释）
-    cout<<"Hello World01"<<endl; // example - 2022/11/03
+    cout<<"Hello World00"<<endl; // example - 2022/11/03
 
     cout<<"xxxxx"<<endl;// test_name - xx/xx/xx
     return 0;
